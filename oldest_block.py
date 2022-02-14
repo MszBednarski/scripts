9609c2aaec470c8dd36f6202ac839a33a1c131cd
MszBednarski <msz.bednarski@gmail.com>
2022-02-14 12:14:46 +0000

    done

---
+++ oldest_block.py
@@ -0,0 +1,12 @@
+import web3
+
+print("gimme node url")
+HTTP = input()
+provider = web3.HTTPProvider(HTTP)
+client = web3.Web3(provider)
+id = client.eth.block_number
+
+while True:
+    id -= 1
+    res = client.eth.getBlock(id)
+    print("The block number is [{}]".format(res["number"]))
