e4d8328a302e06c0d9708a71ae029f017b3110b3
MszBednarski <msz.bednarski@gmail.com>
2022-03-08 14:23:59 +0000

    chmod

--- setup-client.sh
+++ setup-client.sh
@@ -2,6 +2,7 @@
 # on ubuntu 20.04
 export CONFIG=/etc/ssh/sshd_config
 if [ -z ${var+x} ]; then echo "SET HOST ENV VAR FIRST"; exit; fi
+echo "Setting up ssh id and closing the server for password connections."
 ssh -i ~/.ssh/id_rsa ubuntu@$HOST
 sudo echo "ubuntu ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 sudo echo "ChallengeResponseAuthentication no" >> $CONFIG
