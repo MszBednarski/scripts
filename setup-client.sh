5f39932af8f44ceef1a718d576aa0fd18d39fdfc
MszBednarski <msz.bednarski@gmail.com>
2022-03-08 14:49:51 +0000

    asef

--- setup-client.sh
+++ setup-client.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 # on ubuntu 20.04
-export CONFIG=/etc/ssh/sshd_config
-if [ -z ${var+x} ]; then echo "SET HOST ENV VAR FIRST"; exit; fi
+if [ -z ${HOST+x} ]; then echo "SET HOST ENV VAR FIRST"; exit; fi
 echo "Setting up ssh id and closing the server for password connections."
-ssh -i ~/.ssh/id_rsa ubuntu@$HOST
-sudo echo "ubuntu ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-sudo echo "ChallengeResponseAuthentication no" >> $CONFIG
-sudo echo "PasswordAuthentication no" >> $CONFIG
-sudo echo "UsePAM no" >> $CONFIG
-sudo echo "PermitRootLogin no" >> $CONFIG
-sudo echo "PermitRootLogin prohibit-password" >> $CONFIG
-sudo systemctl reload ssh
+ssh-copy-id -i ~/.ssh/id_rsa ubuntu@$HOST
+function s() {
+    ssh ubuntu@$HOST $1
+}
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "Please exec ' echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers && exit'"
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+ssh ubuntu@$HOST
+echo "Setting up the RSA auth."
+s 'echo "ChallengeResponseAuthentication no" | sudo tee -a /etc/ssh/sshd_config'
+s 'echo "PasswordAuthentication no" | sudo tee -a /etc/ssh/sshd_config'
+s 'echo "UsePAM no" | sudo tee -a /etc/ssh/sshd_config'
+s 'echo "PermitRootLogin no" | sudo tee -a /etc/ssh/sshd_config'
+s 'echo "PermitRootLogin prohibit-password" | sudo tee -a /etc/ssh/sshd_config'
+s 'sudo systemctl reload ssh'
